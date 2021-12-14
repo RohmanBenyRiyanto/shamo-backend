82c7bfffec17272c8766b8d4f9c22f40daeba5b9
RohmanBenyRiyanto <rendyasw@gmail.com>
2021-12-14 23:25:35 +0700

    first commit

---
+++ app/Models/TransactionItem.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionItem extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'users_id',
+        'products_id',
+        'transactions_id',
+        'quantity',
+    ];
+
+    public function products()
+    {
+        return $this->hasOne(Product::class, 'id', 'products_id');
+    }
+}
